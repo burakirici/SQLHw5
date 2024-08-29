aafbf115a659a6d8966376e0b34d9c845f9e2504
Burak Kırıcı <burakpinar76@gmail.com>
2024-08-29 10:44:53 +0300

    Add files via upload

---
+++ Homework5.sql
@@ -0,0 +1,23 @@
+-- Question 1
+
+--SELECT * FROM film
+--WHERE title LIKE '%n'
+--ORDER BY length DESC
+--LIMIT 5;
+
+-- Question 2
+
+--SELECT * FROM film
+--WHERE title LIKE '%n'
+--ORDER BY length ASC
+--OFFSET 5
+--LIMIT 5;
+
+-- Question 3
+
+--SELECT * FROM customer
+--WHERE store_id = 1
+--ORDER BY last_name ASC
+--LIMIT 4;
+
+
